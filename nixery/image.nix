a922b95fdb2f6c189ca7de7ddb7b6de399e597a2
Zachary Spar <zspar@coreweave.com>
2026-04-17 16:25:08 +0000

    fix(nixery): put object seekable

--- nixery/image.nix
+++ nixery/image.nix
@@ -5,7 +5,10 @@ let
   patched = pkgs.applyPatches {
     name = "nixery-patched";
     src = ../nixery-src;
-    patches = [ ./patches/s3-use-path-style-env.patch ];
+    patches = [
+      ./patches/s3-use-path-style-env.patch
+      ./patches/s3-putobject-spool-seekable.patch
+    ];
   };
 in
 (import "${patched}/default.nix" { inherit pkgs; }).nixery-image
